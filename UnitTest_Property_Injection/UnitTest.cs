59e82bcb97b89b2004877ec7552ecef0345c011e
Matias Rosenkilde Tvorup <201807748@post.au.dk>
2020-02-12 15:46:51 +0100

    Yeet

--- UnitTest_Property_Injection/UnitTest.cs
+++ UnitTest_Property_Injection/UnitTest.cs
@@ -14,7 +14,8 @@ namespace UnitTest_Property_Injection
         private void SetUp()
         {
             _uut = new ECS(10);
-
+            _uut._heater = new MockHeater();
+            _uut._tempSensor = new StubTempSensor();
 
         }
 
@@ -24,7 +25,39 @@ namespace UnitTest_Property_Injection
 
         }
 
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(-10)]
+        public void Test_Set_Get_Threshold(int n)
+        {
+            _uut.SetThreshold(n);
+
+            Assert.That(_uut.GetThreshold(),Is.EqualTo(n));
+        }
+
+
+
 
 
     }
 }
+
+//public void Regulate()
+//{
+//var t = _tempSensor.GetTemp();
+//    if (t < _threshold)
+//    _heater.TurnOn();
+//    else
+//_heater.TurnOff();
+
+//}
+
+//public int GetCurTemp()
+//{
+//return _tempSensor.GetTemp();
+//}
+
+//public bool RunSelfTest()
+//{
+//return _tempSensor.RunSelfTest() && _heater.RunSelfTest();
+//}
